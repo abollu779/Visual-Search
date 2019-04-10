d9d952c6ea8bf3ff6c1a31e733daaac7ddea4f41
abollu779 <abollu779@gmail.com>
2019-04-10 16:08:49 -0400

    Initial Commit

---
+++ model/Network.py
@@ -0,0 +1,16 @@
+############################################################
+# TODO:
+# Instantiate Network Class that contains all modules
+
+# FORWARD:
+# Visual Input from Dataloader to CNN + Textual Input 
+# from Dataloader to BiLSTM
+
+# Concatenate CNN output + BiLSTM output -> Combined input
+
+# Pass the combined input to the Attention module, 
+# Feature Predictor and BBox CNN
+
+# Obtain three outputs used by main file to compute losses 
+# and train the model
+############################################################
