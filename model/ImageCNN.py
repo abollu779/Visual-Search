7f758384b9b4b6e877d6e5e6974f3bc3c842377c
Pallavi Sharma <pallavisharma@Pallavis-MBP.wv.cc.cmu.edu>
2019-04-19 18:41:58 -0400

    tested for batch of images

--- model/ImageCNN.py
+++ model/ImageCNN.py
@@ -1,14 +1,17 @@
 #################################################################
-#								#
-# Input   : (Currently) Reads image path from command line	#
-#	    (Ideally:)				                #
-#	    Image of shape (batch_size) 3 * 416 * 416		#
-#							        #
-# Returns : Output of 58th convolutional layer of yolov3.	#
-#	    i.e. layer 'conv_80'				#
-#	    (Currently) shape- 1 * 13 * 13 * 1024		#
-#	    (Ideally) shape- (batch_size) 13 * 13 * 1024	#
-#								#
+#                                                               #
+# Input   : (Currently) Reads image path from command line.     #
+#           (Ideally:)                                          #
+#           Array of images(as numpy array of original size or  #
+#           3*416*416)                                          #
+#           OR                                                  #
+#           List of paths of images in a batch                  #
+#                                                               #
+# Returns : Output of 58th convolutional layer of yolov3.       #
+#           i.e. layer 'conv_80'                                #
+#           (Currently) shape- 1 * 13 * 13 * 1024               #
+#           (Ideally) shape- (batch_size) 13 * 13 * 1024.       #
+#                                                               #
 #################################################################
 
 import cv2 as cv
@@ -35,13 +38,15 @@ net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
 if not os.path.isfile(args.image):
     print("Input image file ", args.image, " doesn't exist")
     sys.exit(1)
-cap = cv.VideoCapture(args.image)
-
-hasFrame, frame = cap.read()
+imgs =[]
+img = cv.imread(args.image)
+imgs.append(img)
+imgs = np.asarray(imgs)
 
 # If: Input shape == 3*416*416: No need to create blob
 # Else: Create a 4D blob from a frame.
-blob = cv.dnn.blobFromImage(frame, 1/255, (Width, Height), [0,0,0], 1, crop=False)
+blob = cv.dnn.blobFromImages(imgs, 1/255, (Width, Height), [0,0,0], 1, crop=False)
+print (blob.shape)
 
 # Sets the input to the network
 net.setInput(blob)
@@ -49,3 +54,4 @@ net.setInput(blob)
 # Runs the forward pass to get output of the output layers
 layersNames = net.getLayerNames()
 out = net.forward('conv_80')
+print (out.shape)
