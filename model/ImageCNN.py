7edab1fdf336099888e9151f867c7f1ae44e45fe
Pallavi Sharma <pallavisharma@Pallavis-MBP.wv.cc.cmu.edu>
2019-04-25 03:16:20 -0400

    saving image ID and embedding

--- model/ImageCNN.py
+++ model/ImageCNN.py
@@ -1,16 +1,8 @@
 #################################################################
 #                                                               #
-# Input   : (Currently) Reads image path from command line.     #
-#           (Ideally:)                                          #
-#           Array of images(as numpy array of original size or  #
-#           3*416*416)                                          #
-#           OR                                                  #
-#           List of paths of images in a batch                  #
-#                                                               #
-# Returns : Output of 58th convolutional layer of yolov3.       #
+# Saves :   Output of 58th convolutional layer of yolov3.       #
 #           i.e. layer 'conv_80'                                #
-#           (Currently) shape- 1 * 13 * 13 * 1024               #
-#           (Ideally) shape- (batch_size) 13 * 13 * 1024.       #
+#           shape- (num_images) * 1024 * 13 * 13                #
 #                                                               #
 #################################################################
 
@@ -19,14 +11,14 @@ import argparse
 import sys
 import numpy as np
 import os.path
-from DataLoader import *
+import pickle
+import shelve
+import time
+
+from ImageLoader import *
 
 Width  = 416       #Width of network's input image
 Height = 416      #Height of network's input image
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--image')
-args = parser.parse_args()
         
 # Give the configuration and weight files for the model and load the network using them.
 modelConfiguration = "yolov3.cfg"
@@ -38,24 +30,21 @@ net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-dataset = RefDataset("train")
-loader = DataLoader(dataset, batch_size=128, shuffle=True, num_workers=(8 if device == "cuda" else 0))
+dataset = ImageLoader()
+loader = DataLoader(dataset, batch_size=256, shuffle=False, num_workers=(8 if device == "cuda" else 0))
 
-
-img_yolo_embedding = {}
-for idx, (file_names,file_ids) in enumerate(loader):
-	# print(idx,len(file_names))
-	img_yolo_embedding = img_yolo_embedding.fromkeys(file_ids)
+for idx, (img_names,img_IDs) in enumerate (loader):
+	start = time.time()
+	img_IDs = img_IDs.numpy()
 	imgs =[]
-
-	for f in file_names:
-		f = os.path.join('data/images/train2014',f)
+	for f in img_names:
+		f = os.path.join('data/images/train2014/',f)
 		if not os.path.isfile(f):
-			print("Input image file ", f, " doesn't exist")
+			print("Input image file ", args.image, " doesn't exist")
 			sys.exit(1)
-
 		img = cv.imread(f)
 		imgs.append(img)
+	
 	imgs = np.asarray(imgs)
 
 	# Create a 4D blob from a frame.
@@ -67,9 +56,20 @@ for idx, (file_names,file_ids) in enumerate(loader):
 	
 	# Runs the forward pass to get output of the output layers
 	# layersNames = net.getLayerNames()
-	if idx == 0:
-		out = net.forward('conv_80')
-		print (out.shape)
+	out = net.forward('conv_80')
+
+	if os.path.isfile("ImageIDs.npy") and os.path.isfile("ImageEmbeddings.npy"):
+		ids = np.load("ImageIDs.npy")
+		ids = np.concatenate((ids,img_IDs))
+		np.save("ImageIDs.npy",ids)
+
+		embeddings = np.load("ImageEmbeddings.npy")
+		embeddings = np.concatenate((embeddings,out))
+		np.save("ImageEmbeddings.npy",embeddings)
 	else:
-		out = np.concatenate((out, net.forward('conv_80')), axis=0)
-		print (out.shape)
+		print (not( os.path.isfile("ImageIDs.npy") or os.path.isfile("ImageEmbeddings.npy")))
+		assert(not( os.path.isfile("ImageIDs.npy") or os.path.isfile("ImageEmbeddings.npy")))
+		np.save("ImageIDs.npy",img_IDs)
+		np.save("ImageEmbeddings.npy",out)
+	end = time.time()
+	print ("Batch#",idx,"saved", "time =", end-start)
